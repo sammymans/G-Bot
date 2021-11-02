65e16ba87728ca96b234738ac3f627d16becd7d5
morbe65 <61469436+morbe65@users.noreply.github.com>
2021-11-02 15:39:02 -0400

    Login Complete - could be changed to not use XPATH

--- buttonClicker.py
+++ buttonClicker.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from login import username
+from login import password
+
 
 import time
 
@@ -13,16 +16,27 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://shop.westernmustangs.ca/Program/GetProgramDetails?courseId=b48c8d0b-4493-4d3c-a4ed-9507063a9062&semesterId=81dac0e7-2456-44c9-bfe4-6ed494cc6824")
 
-link = driver.find_element_by_link_text("REGISTER")
-link.click()
+#link = driver.find_element_by_link_text("REGISTER")
+#link.click()
 
 try: 
-    element= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button.loginOption btn btn-lg btn-block btn-social btn-linkedin")))
+    element = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT,"REGISTER")))
     element.click()
+    
+    element= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//*[@id='divLoginOptions']/div[2]/div[2]/div/button")))
+    element.click()
+
+    element= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID,"userId")))
+    element.clear()
+    element.send_keys(username)
+
+    element= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID,"password")))
+    element.clear()
+    element.send_keys(password)
+
+    element= WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//*[@id='fm1']/fieldset/div[3]/input[4]")))
+    element.click()
+
 except:
     driver.quit()
-
-
-
-
 
