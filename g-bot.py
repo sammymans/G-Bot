80e9272fc4f399084dde94ed1b27ad1ccfe88be0
sammymans <sfarnum3@uwo.ca>
2021-11-01 22:38:28 -0400

    update

--- g-bot.py
+++ g-bot.py
@@ -14,22 +14,23 @@ soup= BeautifulSoup(page.content, 'html.parser')
 page.close()
 
 containers =soup.findAll("div", {"class":"caption program-schedule-card-caption"})
-
-for container in containers:
+#for container in containers:
+for count, container in enumerate(containers):
 
     Date= container.h4.span.text
     Time = container.h4.small.text
     Available=container.h4.small.span.text
 
     list = Time.split()
-    
+
     times = ""
     i = 0
-    while not list[i].isdigit():
+    while not list[i].isdigit() and i <= 4:
         times += list[i]
         i += 1
 
-    print("\nDate: "+ Date.strip())
+    print("\nOption " + str(count+1) + ":")
+    print("Date: "+ Date.strip())
     print("Time: "+ times)
     print("Availability: "+ Available.strip())
 
